677cf80e23a1d903926baf650f476fbbc2db0e50
immanwela <111110758+immanwela@users.noreply.github.com>
2023-03-05 16:49:53 +0800

    Update
    php, css, img

---
+++ schedsystem/bakery_navbar.php
@@ -0,0 +1,16 @@
+<div class="navbar" id="myNavbar">
+    <h2 class="logo"> <a href="sched.php">Welcome Easternians!</a> </h2>
+
+    <div id="myBtn" class= "button-Hamburger">
+        <div class="bar1"></div>
+        <div class="bar2"></div>
+        <div class="bar3"></div>
+    </div>
+
+    <ul id="myDropdown" class="navigation">
+        <li><a href="link1.php">H O M E</a></li>
+        <li><a href="link2.php">A B O U T </a></li>
+        <li><a href="link3.php">C O N T A C T</a></li>
+        <li><a href="link4.php">L O G &nbsp;&nbsp; I N</a></li>
+    </ul>
+</div>
