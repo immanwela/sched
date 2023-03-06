8a44838498de34753dee65c9b65caa4354b5ea9b
immanwela <111110758+immanwela@users.noreply.github.com>
2023-03-06 15:00:31 +0800

    contact and login
    update

---
+++ schedsystem/login.php
@@ -0,0 +1,24 @@
+<?php include('bakery_header.php') ?>
+<section>  
+    <div class="wrapper">
+        <div class="forms">
+            <form action="#">
+                    <div class="form">
+                        <h1>USER LOGIN</h1>
+                        <input class="bar" type="text" placeholder="Username" required>
+                        <input class="bar" type="text" placeholder="Password" required>
+                    </div>
+                    <div class="container">
+                        <label><input type="checkbox" checked="checked">Remember me</label>
+                        <span class="pword">Forgot <a href="#">password?</a></span>
+                </div>
+                    <div class="container-btn">
+                            <button class="btn" type="button">REGISTER</button>
+                            <button class="btn" type="submit">LOGIN</button>
+                           
+                    </div>
+                </form>
+            </div>
+    </div>
+</section>
+ 
